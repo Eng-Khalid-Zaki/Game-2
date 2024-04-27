1b50a1d225561339eefea5e907e070e695d05e12
Eng-Khalid-Zaki <eng.khalid.zaki.2022@gmail.com>
2024-04-27 04:40:46 +0300

    Finishing this project

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,18 +1,34 @@
-import { forwardRef } from "react";
-const ResultModal = forwardRef(({ result, targetTime }, ref) => {
-  return (
-    <dialog ref={ref} className="result-modal">
-      <h2>You {result}</h2>
-      <p>
-        The target time was <strong>{targetTime}</strong>
-      </p>
-      <p>
-        You stopped the timer with <strong>X seconds left</strong>
-      </p>
-      <form method="dialog">
-        <button>Close</button>
-      </form>
-    </dialog>
-  );
-});
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+const ResultModal = forwardRef(
+  ({ remainingTime, targetTime, onReset }, ref) => {
+    const dialog = useRef();
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+    useImperativeHandle(ref, () => {
+      return {
+        open() {
+          dialog.current.showModal();
+        },
+      };
+    });
+    return createPortal(
+      <dialog ref={dialog} className="result-modal" onClose={onReset}>
+        {remainingTime <= 0 && <h2>You Lost!</h2>}
+        {remainingTime > 0 && <h2>Your score is {score}</h2>}
+        <p>
+          The target time was <strong>{targetTime}</strong>
+        </p>
+        <p>
+          You stopped the timer with{" "}
+          <strong>{formattedRemainingTime} seconds left</strong>
+        </p>
+        <form method="dialog" onSubmit={onReset}>
+          <button>Close</button>
+        </form>
+      </dialog>,
+      document.getElementById("modal")
+    );
+  }
+);
 export default ResultModal;
--- src/components/TimeChallenge.jsx
+++ src/components/TimeChallenge.jsx
@@ -1,41 +1,47 @@
 import { useState, useRef } from "react";
 import ResultModal from "./ResultModal.jsx";
 export default function TimeChallenge({ title, targetTime }) {
-  const [timeExpired, setTimeExpired] = useState(false);
-  const [startTime, setStartTime] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
   const timer = useRef();
   const dialog = useRef();
+  const timeIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
 
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.open();
+  }
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
   function handleStart() {
-    timer.current = setTimeout(() => {
-      setTimeExpired(true);
-      dialog.current.showModal();
-    }, targetTime * 1000);
-    setStartTime(true);
+    timer.current = setInterval(
+      () => setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10),
+      10
+    );
   }
 
   function handleStop() {
-    clearTimeout(timer.current);
-    setStartTime(false);
+    clearInterval(timer.current);
+    dialog.current.open();
   }
 
   return (
     <>
-      <ResultModal ref={dialog} result="lost" targetTime={targetTime} />
+      <ResultModal
+        ref={dialog}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+        targetTime={targetTime}
+      />
       <section className="challenge">
         <h2>{title}</h2>
-        <p>{timeExpired ? "You lost!" : ""}</p>
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
-        <button onClick={startTime ? handleStop : handleStart}>
-          {startTime && !timeExpired ? "Stop" : "Start"} Challenge
+        <button onClick={timeIsActive ? handleStop : handleStart}>
+          {timeIsActive ? "Stop" : "Start"} Challenge
         </button>
-        <p>
-          {startTime && !timeExpired
-            ? "Time is running..."
-            : "Time is inactive"}
-        </p>
+        <p>{timeIsActive ? "Time is running..." : "Time is inactive"}</p>
       </section>
     </>
   );
